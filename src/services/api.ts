495fe0e9b337e9f61a05596bb1eda009736e30c5
Mohak Golani <148332916+mohak-golani@users.noreply.github.com>
2026-03-16 11:37:02 +0530

    Update BASE_URL to point to production API
    testing

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = "https://lila-backend-93kb.onrender.com/api/matches";
 
 const api = axios.create({
   baseURL: BASE_URL,
